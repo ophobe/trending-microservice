d3b9edd68e729b1518b3e5cd05db84b861e27185
ecrmnn <danieleckermann@gmail.com>
2017-02-14 20:20:06 +0100

    fixes

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const moment = require('moment');
 const trending = require('trending-github');
 const axios = require('axios');
 const endpoint = 'https://api.github.com/repos/ophobe/trending/contents/';
+const languages = require('./languages');
 require('dotenv').config();
 
-app.get('/favicon.ico', function *(req, res) {
-  res.send('OK');
+app.use(function (req, res, next) {
+  if (languages.includes(req.originalUrl.replace(/\//g, ''))) {
+    return next();
+  }
+
+  res.json({error: 'Invalid endpoint'});
 });
 
 app.get('/:language?', function*(req, res) {
@@ -17,21 +22,23 @@ app.get('/:language?', function*(req, res) {
   const repos = yield trending('daily', language).then(response => response);
   const content = new Buffer(JSON.stringify({repositories: repos}, null, 2)).toString('base64');
   const today = moment().format('DD-MM-YYYY');
+  
+  if (process.env.commit !== 'false') {
+    yield axios
+      .put(`${endpoint}${language}/${today}.json`, {
+        message: `${language} @ ${today}`,
+        content
+      }, {
+        auth: {
+          username: process.env.ophobe_email,
+          password: process.env.ophobe_github_access_token
+        }
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
 
-  yield axios
-    .put(`${endpoint}${language}/${today}.json`, {
-      message: `${language} @ ${today}`,
-      content
-    }, {
-      auth: {
-        username: process.env.ophobe_email,
-        password: process.env.ophobe_github_access_token
-      }
-    })
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
-
-  res.send(`${language} committed`);
+  res.json(repos);
 });
 
 app.listen(3000, () => console.log('Application running on http://localhost:3000'));
---
+++ languages.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = [
+  '',
+  'c++',
+  'c',
+  'crystal',
+  'csharp',
+  'css',
+  'dart',
+  'elixir',
+  'go',
+  'html',
+  'java',
+  'javascript',
+  'julia',
+  'objective',
+  'php',
+  'python',
+  'r',
+  'ruby',
+  'rust',
+  'scala',
+  'swift'
+];
